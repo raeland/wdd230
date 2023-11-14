bae5052d9e4f47f7d2a0c285b3e2533497911623
raeland <rsvarady@gmail.com>
2023-11-13 21:50:46 -0500

    js

---
+++ week02/scripts/bom.js
@@ -0,0 +1,8 @@
+const input = documetn.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() { ... });
+button.addEventListener('click', () => { ... });
+
+if (input.value != ' ') { ... }
