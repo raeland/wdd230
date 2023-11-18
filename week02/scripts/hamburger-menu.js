c35385854cb256188a5269be403b3250b46ab69b
raeland <rsvarady@gmail.com>
2023-11-18 04:44:27 -0500

    ham

--- week02/scripts/hamburger-menu.js
+++ week02/scripts/hamburger-menu.js
@@ -4,7 +4,7 @@ const hambutton = document.querySelector('#menu');
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
+	mainnav.classList.toggle('open');
 	hambutton.classList.toggle('open');
 });
 
