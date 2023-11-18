b3d946a3982a5f655a9d01a4e871f0ed7fa54b54
raeland <rsvarady@gmail.com>
2023-11-18 04:35:06 -0500

    bom

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,7 +2,26 @@ const input = documetn.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', function() { ... });
-button.addEventListener('click', () => { ... });
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        const li = document.createElement('li'); 
 
-if (input.value != ' ') { ... };
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        
+        li.textContent = input.value;
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        
+    } else {
+        input.value = '';
+        input.placeholder = "Please Enter a Chapter";
+    }
+});
