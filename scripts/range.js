c84883b7b4031b491009dcc9945f288ede6e75d8
raeland <rsvarady@gmail.com>
2023-11-29 23:33:14 -0500

    range info

---
+++ scripts/range.js
@@ -0,0 +1,9 @@
+const rangeValue = document.getElementById("rangeValue");
+const range = document.getElementById("rating");
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangeValue.innerHTML = range.value;
+}
