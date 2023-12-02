71242ffc0037ea6fc9a17bb3237f3923b113f657
raeland <rsvarady@gmail.com>
2023-12-02 16:08:55 -0500

    displayLinks change

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,7 @@ const linksURL = "https://raeland.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data.lessons);
+    displayLinks(data);
   }
   
   getLinks();
