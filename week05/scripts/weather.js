6ab58d5d39c12ee3c98b792e1fc013b7ea2bb294
raeland <rsvarady@gmail.com>
2023-12-02 17:46:23 -0500

    weather fixed

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=05d35910804aaa8c51aba7e709187c95';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=62f55218bbe7f6e588ad7ec324af1d2a';
 
 async function apiFetch() {
     try {
@@ -27,5 +27,5 @@ apiFetch();
     description = desc.replace(/\b[a-z]/g, match => match.toUpperCase());
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${description}`;
+    captionDesc.textContent = `${desc}`;
   }
