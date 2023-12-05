b128f6256412279613fbad3a279a124ebbe1a2df
raeland <rsvarady@gmail.com>
2023-12-04 21:35:58 -0500

    rewrite from blazzard video

--- scripts/links.js
+++ scripts/links.js
@@ -1,53 +1,55 @@
 const baseURL = 'https://raeland.github.io/wdd230/';
 const linksURL = 'https://raeland.github.io/wdd230/data/links.json';
 
-const learningActivities = document.querySelector('.cardlist');
+const learningActivities = document.querySelector('.card-list');
 
 async function getActivityList() {
     const response = await fetch(linksURL);
     if(response.ok) {
         const data = await response.json();
         //console.log(data);
-        buildLearningCard(weekLinks);
+        buildLearningCard(data);
     } else {
         throw Error(response.statusText);
     }
 }
 
 function buildLearningCard(weekLinks){
-    console.log(weekLinks);
+        const displayLinks = (weeks) => {
+            weeks.forEach((week) => {
+                let card = document.querySelector('div');
+                let h4 = document.createElement('h4');
+                let week = document.createElement('p');
+
+                h4.innerHTML = `${weekLinks.week}`;
+                week.innerHTML = `${week.week}: `;
+
+                card.append(h2);
+                card.appendChild(week);
+                cards.appendChild(card);
+            });
+
+                card.appendChild(learningActivities);
+
+                weeks.forEach((week) => {
+                    let weekElement = document.createElement('p');
+                    weekElement.innerHTML = `${week.week}: `;        
+                    week.links.forEach((link) => {
+                        const linkElement = document.createElement('a');
+                        linkElement.textContent = link.title;
+                        linkElement.href = `${baseURL}${link.url}`;
+        
+                        weekElement.appendChild(linkElement);
+        
+                        const seperator = document.createTextNode(` | `);
+                        weekElement.appendChild(seperator);
+                    })
+                })
+        
+                learningActivities.appendChild(weekElement);
+        
+                weekElement.removeChild(weekElement.lastChild);
+            });
+    };
 }
     getActivityList();
-/*
-    //console.log(activitiesData);
-    displayWeeks(data.weeks);
-};
-getLinks();
-//const displayLinks = (weeks) => {
-    weeks.forEach((week) => {
-        let card = document.querySelector('.cardlist');
-        let title = document.createElement('h3');
-        let learningActivities = document.createElement('div');
-        title.textContent = 'Learning Activities';
-        card.appendChild(title);
-        card.appendChild(learningActivities);
-        weeks.forEach((week) => {
-            let weekElement = document.createElement('p');
-            weekElement.textContent = `${weel.week}: `;        
-            week.links.forEach((link) => {
-                const linkElement = document.createElement('a');
-                linkElement.textContent = link.title;
-                linkElement.href = `${baseURL}${link.url}`;
-
-                weekElement.appendChild(linkElement);
-
-                const seperator = document.createTextNode(` | `);
-                weekElement.appendChild(seperator);
-            })
-        })
-
-        learningActivities.appendChild(weekElement);
-
-        weekElement.removeChild(weekElement.lastChild);
-    });
-}*/
