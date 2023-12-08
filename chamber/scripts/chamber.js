1933655ad69fffd9d31a3b22847cc4a0a4d45598
raeland <rsvarady@gmail.com>
2023-12-07 20:16:14 -0500

    removed extra date validators

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,17 +1,3 @@
-const hambutton = document.querySelector('#menu');
-const mainnav = document.querySelector('nav ul');
-
-
-hambutton.addEventListener('click', () => {
-    mainnav.classList.toggle('open');
-    hambutton.classList.toggle('open');
-})
-
-
-const currentYearElement = document.getElementById('currentYear');
-const currentYear = new Date().getFullYear();
-currentYearElement.textContent = currentYear;
-
 const lastModifiedElement = document.getElementById('lastModified');
 const lastModifiedDate = new Date(document.lastModified);
 lastModifiedElement.textContent = 'Last modified: ' + lastModifiedDate.toDateString();
