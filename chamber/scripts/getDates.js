8a63c4f80f7144f825171ffe1319e75d627f434b
raeland <rsvarady@gmail.com>
2023-12-07 20:11:43 -0500

    hidden date????

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -4,3 +4,11 @@ document.querySelector("#currentYear").textContent = currentYear;
 let lastModif = new Date(document.lastModified);
 lastModified = `Last Modification:  ${lastModif}`;
 document.querySelector("#lastModified").textContent = lastModified;
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function () {
+    const currentTimestamp = new Date().toISOString();
+    document.getElementById('timestamp').value = currentTimestamp;
+});
+localStorage.setItem('lastVisit', new Date().toISOString());
